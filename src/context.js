f56e84baa1422815ebb159043de45838249a561d
Alfalmi <pinooxd@gmail.com>
2021-09-01 01:20:49 -0700

    search room filters add

--- src/context.js
+++ src/context.js
@@ -71,41 +71,45 @@ export default class RoomProvider extends Component {
   };
 
   filterRooms = () => {
-    let {
-      rooms,
-      type,
-      capacity,
-      price,
-      minSize,
-      maxSize,
-      breakfast,
-      pets
-    } = this.state;
-// all the rooms
-let tempRooms = [...rooms];
-// transform value
-capacity = parseInt(capacity)
-price = parseInt(price)
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state;
+    // all the rooms
+    let tempRooms = [...rooms];
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
 
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
 
-// filter by type
-if(type !== 'all'){
-  tempRooms = tempRooms.filter(room => room.type === type)
-}
-// filter by capacity
-if (capacity !== 1) {
-  tempRooms = tempRooms.filter(room => room.capacity >= capacity);
-}
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
 
-// filter by price
-tempRooms = tempRooms.filter(room => room.price <= price)
+    // filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
 
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
 
-// change state
-this.setState({
-  sortedRooms:tempRooms
-})
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
 
+    // change state
+    this.setState({
+      sortedRooms: tempRooms,
+    });
   };
 
   render() {
